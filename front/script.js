ed4d4f71b19c6ec94ba5d6c29ee2b86697c31cc3
Laura <pizzuttilaura@gmail.com>
2024-04-16 09:37:39 -0300

    api

---
+++ front/script.js
@@ -0,0 +1,21 @@
+let button = document.getElementById("handleSubmit");
+
+button.onclick = async function () {
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description").value;
+    let data = (title,description)
+
+    const response = await fetch("http://localhost:3001/api/store/task", {
+        method: "POST",
+        headers: {"Content-type": "application/json;charset=UTF-8"},
+        body: JSON.stringify(data)
+    });
+
+    let Content = await response.json();
+
+    if (this.contentEditable.success) {
+            alert("sucesso")
+    } else {
+        alert("não")
+    }
+}
